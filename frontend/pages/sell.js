34a93fde8856753c1a16ed8e05c59466e4aa243e
bodrovphone <bodrovphone@gmail.com>
2019-05-09 21:30:21 +0300

    add a couple of pages + gitignore file (ls3)

---
+++ frontend/pages/sell.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function Sell() {
+  return (
+    <div>
+      Sell
+      <br/>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </div>
+  )
+}
